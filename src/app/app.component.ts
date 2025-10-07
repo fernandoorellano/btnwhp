371c614e97fe70b14b79e00639cc430cf3b66f80
fernandoorellano <fm_orellano@outlook.com>
2025-10-06 21:18:13 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'whatsapp';
+  url = "https://wa.me/+5491130479419?text=pago&attachment=https://media.istockphoto.com/id/1927164716/es/vector/letras-a-mano-de-caligraf%C3%ADa-te-amo-en-coraz%C3%B3n-grunge-inscripci%C3%B3n-te-amo-en-espa%C3%B1ol-tarjeta.jpg?s=612x612&w=0&k=20&c=IERP6VJ1ea8UO3hvZvWkWWvnKdAud4QEtQYoavH9yG0=";
+
+
+constructor(private router: Router){}
+
+
+  enviar(){
+    console.log("hola");
+    window.open(this.url, "_blank");
+  }
+
+
+
+
+
 }
+
